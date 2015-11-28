6443194e64d2204d1d120b341383c87e4ccf141a
BitMasterZ <adlakhashobhit@gmail.com>
2015-11-28 02:13:55 -0500

    Generalize and Dominate(will hopefully have a level construction kit):alien:

---
+++ Java/2DRPG/GridMap.java
@@ -0,0 +1,77 @@
+import java.awt.*;
+
+/**
+*@author Shobhit
+*@version 3.0
+*/
+public class GridMap{
+	// 2D Array that stores the fill of each blocks
+	private int GRID[][];
+	// Number of X and Y Blocks in the Map
+	private int XBLOCKS, YBLOCKS;
+	// The size of each block
+	private int BSIZE = 50;
+	private Color FILLS[] = {Color.WHITE, Color.BLACK, Color.RED};
+
+	/**
+	* Initialize the GridMap using the number of blocks and block size
+	* @param X_ Number of X Blocks
+	* @param Y_ Number of Y Blocks
+	* @param Size_ Block size
+	*/
+	public GridMap(int X_, int Y_, int Size_){
+		//Intialize Vars
+		XBLOCKS = X_;
+		YBLOCKS = Y_;
+		BSIZE = Size_;
+		GRID = new int[YBLOCKS][XBLOCKS];
+
+		//Set the Grid fills to 0 [intially]
+		for(int i = 0; i < GRID.length; i++){
+			for(int j = 0; j < GRID[i].length; j++){
+				GRID[i][j] = 0; 
+			}
+		}
+
+		GRID[YBLOCKS/2][XBLOCKS/2] = 1 ;
+
+	}
+
+	public GridMap(int grid[][], int Size_){
+		//Intialize Vars
+		BSIZE = Size_;
+		GRID = grid;
+		XBLOCKS = grid[0].length;
+		YBLOCKS = grid.length;
+	}
+
+
+	/**
+	* Draw a minimap to the screen
+	* @param g Graphics of the component to be drawn in
+	*/
+	public void MDraw(Graphics g, int Dx, int Dy, double Scale){
+		//For each block
+		for(int i = 0; i < GRID.length; i++){
+			for(int j = 0; j < GRID[i].length; j++){
+				//Draw the outline of the block
+				g.setColor(FILLS[GRID[i][j]]);
+				g.fill3DRect((int)(j * BSIZE * Scale + Dx), 
+				    (int) (i * BSIZE * Scale + Dy), (int)(BSIZE * Scale), 
+				    (int)(BSIZE * Scale), true);
+				g.setColor(Color.black);
+				g.draw3DRect((int)(j * BSIZE * Scale + Dx), 
+				    (int) (i * BSIZE * Scale + Dy), (int)(BSIZE * Scale), 
+				    (int)(BSIZE * Scale), true);
+			}
+		}		
+	}
+
+	public int getBSize(){return BSIZE;}
+
+	public int getFill(int i, int j){return GRID[i][j];}
+	
+	public int getXBlocks(){return XBLOCKS;}
+	public int getYBlocks(){return YBLOCKS;}
+
+}
